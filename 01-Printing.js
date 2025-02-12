9f30ecb91501e19241ab81d8d8703c55a1136b82
Goutam Patel <goutampatel9131@gmail.com>
2025-02-12 14:05:01 +0530

    01-Printing

---
+++ 01-Printing.js
@@ -0,0 +1,14 @@
+console.log("Hello JS");
+
+// 2nd way
+// This One is printing without Spacing in them
+process.stdout.write("Hello JS");
+process.stdout.write("Hello JS");
+
+// other ways
+// Printhing like table in it
+console.table({City: "Jaipur"});
+
+// It shows warning like it highlight the text
+console.warn("Warning");
+
