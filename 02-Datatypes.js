2eb7c3dadc2d7fb41049f0a991dd3ded58a1edd0
Goutam Patel <goutampatel9131@gmail.com>
2025-02-12 14:35:36 +0530

    02-datatypes

---
+++ 02-Datatypes.js
@@ -0,0 +1,29 @@
+//Datatypes
+// String
+// Numbers
+// Boolean
+// undefined
+// Null
+// Zero(0)
+
+//Let And Var bhot has diffrenet UseCases
+var Score = 120;
+console.log(Score);
+
+let Run = 130;
+console.log(Run);
+
+
+let name = "Google";
+let isLogin = false;
+
+//Array
+let searchEng = ["Google", "Yahoo", "Duck Duck Go"];
+console.log(searchEng);
+
+//Obj
+let obj = {firstName: "raj", lastName: "malhotra"};
+console.log(obj.firstName);
+console.log(obj.lastName);
+
+
